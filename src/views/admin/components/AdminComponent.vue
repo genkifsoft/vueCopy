4946efa13241f3cb699da1e3d6d100d55965b849
Dang Quy <dangquy95php@gmail.com>
2020-03-30 21:37:34 +0700

    add

--- src/views/admin/components/AdminComponent.vue
+++ src/views/admin/components/AdminComponent.vue
@@ -10,10 +10,15 @@
     </div>
 </template>
 
-<style>
-  @import 'public/plugins/fontawesome-free/css/all.min.css';
-
-</style>
+<style src="../../../assets/plugins/fontawesome-free/css/all.min.css"></style>
+<style href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css"></style>
+<style src="../../../assets/plugins/tempusdominus-bootstrap-4/css/tempusdominus-bootstrap-4.min.css"></style>
+<style src="../../../assets/plugins/icheck-bootstrap/icheck-bootstrap.min.css"></style>
+<style src="../../../assets/plugins/jqvmap/jqvmap.min.css"></style>
+<style src="../../../assets/dist/css/adminlte.min.css"></style>
+<style src="../../../assets/plugins/overlayScrollbars/css/OverlayScrollbars.min.css"></style>
+<style src="../../../assets/plugins/daterangepicker/daterangepicker.css"></style>
+<style src="../../../assets/plugins/summernote/summernote-bs4.css"></style>
 
 <script>
 
@@ -40,3 +45,4 @@ export default {
     }
 };
 </script>
+<script src="../../../assets/plugins/jquery/jquery.min.js"></script>
