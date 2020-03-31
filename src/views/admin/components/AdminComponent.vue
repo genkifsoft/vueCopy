77e6af580a5d73d37aa5636693e8035e78042e8c
Dang Quy <dangquy95php@gmail.com>
2020-04-01 00:02:28 +0700

    add

--- src/views/admin/components/AdminComponent.vue
+++ src/views/admin/components/AdminComponent.vue
@@ -11,7 +11,41 @@
 </template>
 <script>
 
-import '@/assets/plugins/jquery/jquery.js';
+import "@/assets/plugins/jquery/jquery.min.js"
+
+import "@/assets/plugins/jquery-ui/jquery-ui.min.js";
+
+$.widget.bridge('uibutton', $.ui.button)
+
+// <!-- Bootstrap 4 -->
+import "@/assets/plugins/bootstrap/js/bootstrap.bundle.min.js";
+// <!-- ChartJS -->
+import "@/assets/plugins/chart.js/Chart.min.js";
+
+// <!-- Sparkline -->
+import "@/assets/plugins/sparklines/sparkline.js";
+// <!-- JQVMap -->
+import "@/assets/plugins/jqvmap/jquery.vmap.min.js";
+
+import "@/assets/plugins/jqvmap/maps/jquery.vmap.usa.js";
+// <!-- jQuery Knob Chart -->
+import "@/assets/plugins/jquery-knob/jquery.knob.min.js";
+// <!-- daterangepicker -->
+// import "@/assets/plugins/moment/moment.min.js";
+import "@/assets/plugins/daterangepicker/daterangepicker.js";
+// <!-- Tempusdominus Bootstrap 4 -->
+import "@/assets/plugins/tempusdominus-bootstrap-4/js/tempusdominus-bootstrap-4.min.js";
+// <!-- Summernote -->
+import "@/assets/plugins/summernote/summernote-bs4.min.js";
+// <!-- overlayScrollbars -->
+import "@/assets/plugins/overlayScrollbars/js/jquery.overlayScrollbars.min.js";
+// <!-- AdminLTE App -->
+import "@/assets/dist/js/adminlte.js";
+// <!-- AdminLTE dashboard demo (This is only for demo purposes) -->
+import "@/assets/dist/js/pages/dashboard.js";
+// <!-- AdminLTE for demo purposes -->
+import "@/assets/dist/js/demo.js";
+
 import AdminFooterElement from './footer';
 import AdminSlidebarLeft from './slidebar-left';
 import AdminSlidebarRight from './slidebar-right';
